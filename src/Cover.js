6eb4c2def5b32c1768cdcc74ca7c9c618eac7d84
yobibae <bj708012@gmail.com>
2022-01-13 14:52:50 +0900

    facebook share

--- src/Cover.js
+++ src/Cover.js
@@ -1,6 +1,7 @@
 import './Cover.css';
 
 function Cover() {
+  const url = document.location.href
   return (
     <div className="cover px-4 py-5 text-center">
       <img className="d-block mx-auto yoga-logo" src={`${process.env.PUBLIC_URL}/images/home.png`} alt="" />
@@ -9,7 +10,11 @@ function Cover() {
       <img className="author" src={`${process.env.PUBLIC_URL}/images/yobi.jpg`} />
       <ul className="list-unstyled cover-social-actions">
         <li><i className="bi bi-envelope"></i></li>
-        <li><i className="bi bi-facebook"></i></li>
+        <li>
+          <a href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} target="_blank" rel="noreferrer">
+            <i className="bi bi-facebook"></i>
+          </a>
+        </li>
         <li><i className="bi bi-twitter"></i></li>
       </ul>
   </div>
